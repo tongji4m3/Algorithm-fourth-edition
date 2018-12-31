d1e0ccf67d747b6b3878f3c87cf791c75f7c34e8
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2018-12-31 16:00:27 +0800

    Add files via upload

---
+++ 1.3/Exe_1_3_9.java
@@ -0,0 +1,30 @@
+package com.tongji.test_1_3;
+
+import edu.princeton.cs.algs4.Stack;
+
+public class Exe_1_3_9
+{
+	public static void main(String[] args)
+	{
+		String s="1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
+		Stack<String> vals=new Stack<>();
+		Stack<String> ops=new Stack<>();
+		String [] strings=s.split("\\s+");
+		for (int i = 0; i <strings.length ; i++) 
+		{
+			if(strings[i].equals("+")||strings[i].equals("-")||strings[i].equals("*")||strings[i].equals("/")||strings[i].equals("sqrt"))
+				ops.push(strings[i]);
+			else if(strings[i].equals(")"))
+			{
+				String string="(";
+				String v=vals.pop();//右边的
+				String w=vals.pop();//左边的操作数
+				string+=w+ops.pop()+v+")";
+				vals.push(string);
+			}
+			else 
+				vals.push(strings[i]);
+		}
+		System.out.println(vals.pop());
+	}
+}
