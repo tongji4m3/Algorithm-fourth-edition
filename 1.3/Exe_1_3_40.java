6cefdfbaec1636b4ec3df5a2734a94a729882f43
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2019-07-27 10:11:51 +0800

    Update Exe_1_3_40.java

--- 1.3/Exe_1_3_40.java
+++ 1.3/Exe_1_3_40.java
@@ -1,35 +1,37 @@
 package com.tongji.test_1_3;
 
-
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Stack;
-
-public class Exe_1_3_40
+public class MoveToFront
 {
-	private Node first;
-	private int N;
 	private class Node
 	{
 		Character ch;
 		Node next;
 	}
+	
+	private Node first;
+	private int N;
+	
 	public void push(Character ch)
 	{
-		N++;
+		++N;
 		Node oldfirst=first;
 		first=new Node();
 		first.ch=ch;
 		first.next=oldfirst;
 	}
+	
 	public boolean isRepeat(char ch)
 	{
-		for (Node x = first;  x!=null ; x=x.next) 
+		for(Node x=first;x!=null;x=x.next)
 		{
 			if(x.ch==ch)
+			{
 				return true;
+			}
 		}
 		return false;
 	}
+	
 	public boolean isEmpty()
 	{
 		return N==0;
@@ -38,59 +40,39 @@ public class Exe_1_3_40
 	{
 		return N;
 	}
-	public void delete(char ch)
-	{
-		int k=0;
-		for (Node x = first;  x!=null ; x=x.next) 
-		{
-			k++;
-			if(x.ch==ch)
-				break;
-		}
-		//´Ó1ËãÆð
-		if(isEmpty() || k<=0 || k>N)
-			throw new RuntimeException();
-		N--;
-		if(k==1)
-		{
-			first=first.next;
-			return;
-		}
-		Node current=first;
-		int i;
-		for ( i = 0; i < k-2 && current!=null ; i++) 
-		{
-			current=current.next;
-		}
-		if(current!=null && current.next!=null && i==k-2 )
-		{
-			current.next=current.next.next;
-		}
-	}
+	
 	public void print()
 	{
 		for(Node x=first;x!=null;x=x.next)
 			System.out.print(x.ch+" ");
 	}
+	
+	public void delete(char ch)
+	{
+		for(Node x=first;x!=null;x=x.next)
+		{
+			if(x.next!=null && x.next.ch==ch)
+			{
+				x.next=x.next.next;
+				return;
+			}
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		char [] chars= {'a','b','c','d','e','a','e'};
-		Exe_1_3_40 link=new Exe_1_3_40();
-		for (int i = 0; i <chars.length ; i++) 
+		MoveToFront link=new MoveToFront();
+		int N=chars.length;
+		for (int i = 0; i < N; ++i)
 		{
-			if(link.isEmpty())
-				link.push(chars[i]);
-			else
+			if(link.isRepeat(chars[i]))
 			{
-				if(link.isRepeat(chars[i]))
-				{
-					link.delete(chars[i]);
-					link.push(chars[i]);
-				}
-				else
-					link.push(chars[i]);
+				link.delete(chars[i]);
 			}
+			link.push(chars[i]);
 		}
 		link.print();
 	}
 }
+
