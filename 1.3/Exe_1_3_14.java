7b14b83974638cc4681395619870ae1901f78deb
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2019-09-28 19:15:22 +0800

    Update Exe_1_3_14.java

--- 1.3/Exe_1_3_14.java
+++ 1.3/Exe_1_3_14.java
@@ -6,101 +6,108 @@ import java.util.Scanner;
 
 public class Exe_1_3_14<Item> implements Iterable<Item>
 {
-	private Item [] q;
-	private int n;
-	private int first;
-	private int last;
-	
-	public Exe_1_3_14()
-	{
-		q=(Item [])new Object[2];
-	}
-	public boolean isEmpty()
-	{
-		return n==0;
-	}
-	public int size()
-	{
-		return n;
-	}
-	private void resize(int capacity)
-	{
-		assert capacity>=n;
-		Item [] temp=(Item [])new Object[capacity];
-		for(int i=0;i<n;i++)
-		{
-			temp[i]=q[(first+i)%q.length];
-		}
-		q=temp;
-		first=0;
-		last=n;
-	}
-	public void enqueue(Item item)
-	{
-		if(n==q.length)
-			resize(2*q.length);
-		q[last++]=item;
-		if(last==q.length)
-			last=0;
-		n++;
-	}
-	public Item dequeue()
-	{
-		if(isEmpty())
-			throw new NoSuchElementException();
-		Item item=q[first];
-		q[first]=null;
-		n--;
-		first++;
-		if(first==q.length)
-			first=0;
-		if(n>0&&n==q.length/4)
-			resize(q.length/2);
-		return item;
-	}
-	public Item peek()
-	{
-		if(isEmpty())
-			throw new NoSuchElementException();
-		return q[first];
-	}
-	public Iterator<Item> iterator()
-	{
-		return new ArrayIterator();
-	}
-	private class ArrayIterator implements Iterator<Item>
-	{
-		private int i=0;
-		public boolean hasNext()
-		{
-			return i<n;
-		}
-		public void remove()
-		{
-			throw new UnsupportedOperationException();
-		}
-		public Item next()
-		{
-			if(!hasNext())
-				throw new NoSuchElementException();
-			Item item=q[(i+first)%q.length];
-			i++;
-			return item;
-		}
-	}
-	public static void main(String[] args)
-	{
-		Exe_1_3_14<String> queue=new Exe_1_3_14<>();
-		Scanner scanner=new Scanner(System.in);
-		while(scanner.hasNext())
-		{
-			String item=scanner.next();
-			if(!item.equals("-"))
-				queue.enqueue(item);
-			else if(!queue.isEmpty())
-				System.out.print(queue.dequeue()+" ");
-		}
-		System.out.println(queue.size());
-	}
+    private Item[] q;
+    private int N;
+    private int first, last;//last指向实际元素的下一个,类似N
+
+    public ResizingArrayQueue()
+    {
+        q = (Item[]) new Object[2];
+    }
+
+    public boolean isEmpty()
+    {
+        return N == 0;
+    }
+
+    public int size()
+    {
+        return N;
+    }
+
+    private void resize(int capacity)
+    {
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < N; i++)
+        {
+            temp[i] = q[(first + i) % q.length];
+        }
+        q = temp;
+        first = 0;
+        last=N;
+    }
+
+    public void enqueue(Item item)
+    {
+        if(N==q.length)
+        {
+            resize(2*q.length);
+        }
+        q[last++] = item;
+        if(last==q.length)
+        {
+            last = 0;
+        }
+        ++N;
+    }
+    public Item dequeue()
+    {
+        if(isEmpty())
+        {
+            throw new RuntimeException();
+        }
+        Item item = q[first];
+        q[first] = null;
+        --N;
+        ++first;
+        if(first==q.length)
+        {
+            first = 0;
+        }
+        if(N>0 && N==q.length/4)
+        {
+            resize(q.length / 2);
+        }
+        return item;
+    }
+
+    public Item peek()
+    {
+        if(isEmpty())
+        {
+            throw new RuntimeException();
+        }
+        return q[first];
+    }
+
+    public Iterator<Item> iterator()
+    {
+        return new Iterator<Item>()
+        {
+            private int i=0;
+            @Override
+            public boolean hasNext()
+            {
+                return i<N;
+            }
+
+            public void remove()
+            {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public Item next()
+            {
+                if(isEmpty())
+                {
+                    throw new RuntimeException();
+                }
+                Item item = q[(i + first) % q.length];
+                ++i;
+                return item;
+            }
+        };
+    }
 }
 
